7849888f52bc052f15c830f1b30840a92e94cfe0
nguyenminhthong2020 <1612674@student.hcmus.edu.vn>
2021-07-21 12:05:30 +0700

    update

--- template/stock-predicthcmus/stock_app.py
+++ template/stock-predicthcmus/stock_app.py
@@ -16,63 +16,6 @@ server = app.server
 
 scaler = MinMaxScaler(feature_range=(0, 1))
 
-# Read data from CSV file
-df_nse = pd.read_csv("./out/stock_data_example.csv")
-
-df_nse["Date"] = pd.to_datetime(df_nse.Date, format="%Y-%m-%d")
-df_nse.index = df_nse['Date']
-
-
-data = df_nse.sort_index(ascending=True, axis=0)
-new_data = pd.DataFrame(index=range(0, len(df_nse)), columns=['Date', 'Close'])
-
-for i in range(0, len(data)):
-    new_data["Date"][i] = data['Date'][i]
-    new_data["Close"][i] = data["Close"][i]
-
-new_data.index = new_data.Date
-new_data.drop("Date", axis=1, inplace=True)
-
-dataset = new_data.values
-
-train = dataset[0:987, :]
-valid = dataset[987:, :]
-
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(dataset)
-
-x_train, y_train = [], []
-
-for i in range(60, len(train)):
-    x_train.append(scaled_data[i-60:i, 0])
-    y_train.append(scaled_data[i, 0])
-
-x_train, y_train = np.array(x_train), np.array(y_train)
-
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-model = load_model("saved_model.h5")
-
-inputs = new_data[len(new_data)-len(valid)-60:].values
-inputs = inputs.reshape(-1, 1)
-inputs = scaler.transform(inputs)
-
-X_test = []
-for i in range(60, inputs.shape[0]):
-    X_test.append(inputs[i-60:i, 0])
-X_test = np.array(X_test)
-
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-closing_price = model.predict(X_test)
-closing_price = scaler.inverse_transform(closing_price)
-
-train = new_data[:987]
-valid = new_data[987:]
-valid['Predictions'] = closing_price
-
-
-df = pd.read_csv("./out/stock_data_example.csv")
-
 # the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
     "position": "fixed",
@@ -101,9 +44,10 @@ radioitemsPP = dbc.FormGroup(
                 {"label": "XGBoost", "value": "XGBoost"},
                 {"label": "RNN", "value": "RNN"},
                 {"label": "LSTM", "value": "LSTM"},
+                {"label": "ARIMA", "value": "ARIMA"},
                 {"label": "Transformer and Time Embeddings", "value": "TSF & TE"},
             ],
-            value=1,
+            value="XGBoost",
             id="radioitems-input",
         ),
     ]
@@ -127,7 +71,7 @@ submenu_1 = [
     # we use the Collapse component to hide and reveal the navigation links
     dbc.Collapse(
         [
-         dbc.Form([radioitemsPP]),
+            dbc.Form([radioitemsPP]),
         ],
         id="submenu-1-collapse",
     ),
@@ -146,6 +90,7 @@ checklistDT = dbc.FormGroup(
     ]
 )
 
+
 submenu_2 = [
     html.Li(
         dbc.Row(
@@ -162,7 +107,7 @@ submenu_2 = [
     ),
     dbc.Collapse(
         [
-               dbc.Form([checklistDT]),
+            dbc.Form([checklistDT]),
         ],
         id="submenu-2-collapse",
     ),
@@ -183,125 +128,30 @@ app.layout = html.Div([
     html.Div(id="page-content", style=CONTENT_STYLE),
 
     html.Div([
-        html.H1(id="dash-title",
+        html.H1("Stock Price Analysis Dashboard",
                 style={"textAlign": "center"}),
+        html.Hr(),
         html.Div([
-            html.H2("Actual closing price", style={"textAlign": "center"}),
-            dcc.Graph(
-                id="Actual Data",
-                figure={
-                    "data": [
-                        go.Scatter(
-                            x=train.index,
-                            y=valid["Close"],
-                            mode='markers'
-                        )
-                    ],
-                    "layout":go.Layout(
-                        title='scatter plot',
-                        xaxis={'title': 'Date'},
-                        yaxis={'title': 'Closing Rate'}
-                    )
-                }
-            ),
-            html.H2("LSTM Predicted closing price",
+            html.H3(id="dash-title",
                     style={"textAlign": "center"}),
-            dcc.Graph(
-                id="Predicted Data",
-                figure={
-                    "data": [
-                        go.Scatter(
-                            x=valid.index,
-                            y=valid["Predictions"],
-                            mode='markers'
-                        )
 
-                    ],
-                    "layout":go.Layout(
-                        title='scatter plot',
-                        xaxis={'title': 'Date'},
-                        yaxis={'title': 'Closing Rate'}
-                    )
-                }
-
-            )
-        ]),
+            dcc.Dropdown(id='my-dropdown',
+                         options=[{'label': 'Apple', 'value': 'AAPL'},
+                                  {'label': 'Tesla', 'value': 'TSLA'},
+                                  {'label': 'IBM', 'value': 'IBM'}],
+                         multi=True, value=['AAPL'],
+                         style={"display": "block", "margin-left": "auto",
+                                "margin-right": "auto", "width": "60%"}),
+             dcc.Graph(id='compare'),
+             ], className="container"),
     ],
         style=MAIN_STYLE,
     ),
 ])
 
-
-@app.callback(Output('highlow', 'figure'),
-              [Input('my-dropdown', 'value')])
-def update_graph(selected_dropdown):
-    dropdown = {"TSLA": "Tesla", "AAPL": "Apple",
-                "FB": "Facebook", "MSFT": "Microsoft", }
-    trace1 = []
-    trace2 = []
-    for stock in selected_dropdown:
-        trace1.append(
-            go.Scatter(x=df[df["Stock"] == stock]["Date"],
-                       y=df[df["Stock"] == stock]["High"],
-                       mode='lines', opacity=0.7,
-                       name=f'High {dropdown[stock]}', textposition='bottom center'))
-        trace2.append(
-            go.Scatter(x=df[df["Stock"] == stock]["Date"],
-                       y=df[df["Stock"] == stock]["Low"],
-                       mode='lines', opacity=0.6,
-                       name=f'Low {dropdown[stock]}', textposition='bottom center'))
-    traces = [trace1, trace2]
-    data = [val for sublist in traces for val in sublist]
-    figure = {'data': data,
-              'layout': go.Layout(colorway=["#5E0DAC", '#FF4F00', '#375CB1',
-                                            '#FF7400', '#FFF400', '#FF0056'],
-                                  height=600,
-                                  title=f"High and Low Prices for {', '.join(str(dropdown[i]) for i in selected_dropdown)} Over Time",
-                                  xaxis={"title": "Date",
-                                         'rangeselector': {'buttons': list([{'count': 1, 'label': '1M',
-                                                                             'step': 'month',
-                                                                             'stepmode': 'backward'},
-                                                                            {'count': 6, 'label': '6M',
-                                                                             'step': 'month',
-                                                                             'stepmode': 'backward'},
-                                                                            {'step': 'all'}])},
-                                         'rangeslider': {'visible': True}, 'type': 'date'},
-                                  yaxis={"title": "Price (USD)"})}
-    return figure
-
-
-@app.callback(Output('volume', 'figure'),
-              [Input('my-dropdown2', 'value')])
-def update_graph(selected_dropdown_value):
-    dropdown = {"TSLA": "Tesla", "AAPL": "Apple",
-                "FB": "Facebook", "MSFT": "Microsoft", }
-    trace1 = []
-    for stock in selected_dropdown_value:
-        trace1.append(
-            go.Scatter(x=df[df["Stock"] == stock]["Date"],
-                       y=df[df["Stock"] == stock]["Volume"],
-                       mode='lines', opacity=0.7,
-                       name=f'Volume {dropdown[stock]}', textposition='bottom center'))
-    traces = [trace1]
-    data = [val for sublist in traces for val in sublist]
-    figure = {'data': data,
-              'layout': go.Layout(colorway=["#5E0DAC", '#FF4F00', '#375CB1',
-                                            '#FF7400', '#FFF400', '#FF0056'],
-                                  height=600,
-                                  title=f"Market Volume for {', '.join(str(dropdown[i]) for i in selected_dropdown_value)} Over Time",
-                                  xaxis={"title": "Date",
-                                         'rangeselector': {'buttons': list([{'count': 1, 'label': '1M',
-                                                                             'step': 'month',
-                                                                             'stepmode': 'backward'},
-                                                                            {'count': 6, 'label': '6M',
-                                                                             'step': 'month',
-                                                                             'stepmode': 'backward'},
-                                                                            {'step': 'all'}])},
-                                         'rangeslider': {'visible': True}, 'type': 'date'},
-                                  yaxis={"title": "Transactions Volume"})}
-    return figure
-
 # this function is used to toggle the is_open property of each Collapse
+
+
 def toggle_collapse(n, is_open):
     if n:
         return not is_open
@@ -336,15 +186,77 @@ for i in [1, 2]:
     ],
 )
 def on_form_change(radio_items_value, checklist_value):
-    template = "Phương pháp {} - {} đặc trưng"
+    template = 'Phương pháp {} - Đặc trưng [{}]'
+    temp = ['Close', 'Price Of Change']
+    title = ''
+    for i in range(len(checklist_value)):
+        if(i == 0):
+            title += temp[checklist_value[i]-1]
+        else:
+            title += ', ' + temp[checklist_value[i]-1]
 
-    n_checkboxes = len(checklist_value)
+    # load file & update graph
+    # TODO
 
     output_string = template.format(
         radio_items_value,
-        n_checkboxes,
+        title,
     )
     return output_string
+
+
+@app.callback(Output('compare', 'figure'),
+              [
+    Input('my-dropdown', 'value'),
+    Input("radioitems-input", "value"),
+    Input("checklist-input", "value"),
+])
+def update_graph(selected_dropdown, radio_items_value, checklist_value):
+    dropdown = {"TSLA": "Tesla", "AAPL": "Apple", "IBM": "IBM", }
+    trace1 = []
+    trace2 = []
+    for stock in selected_dropdown:
+        # TODO load file
+        method = ''
+        for i in checklist_value:
+            method += str(i)
+        filename = './out/'+stock + '_' + radio_items_value + '_' + method + '.csv'
+
+        # df = pd.read_csv("./out/AAPL_ARIMA_1.csv")
+        df = pd.read_csv(filename)
+        df.head()
+        df["Date"] = pd.to_datetime(df.Date, format="%Y-%m-%d")
+        df.index = df['Date']
+
+        # Load figure
+        trace1.append(
+            go.Scatter(x=df.index,
+                       y=df["Prediction"],
+                       mode='lines', opacity=0.7,
+                       name=f'Prediction {dropdown[stock]}', textposition='bottom center'))
+        trace2.append(
+            go.Scatter(x=df.index,
+                       y=df["Close"],
+                       mode='lines', opacity=0.6,
+                       name=f'Actual {dropdown[stock]}', textposition='bottom center'))
+    traces = [trace1, trace2]
+    data = [val for sublist in traces for val in sublist]
+    figure = {'data': data,
+              'layout': go.Layout(colorway=["#5E0DAC", '#FF4F00', '#375CB1',
+                                            '#FF7400', '#FFF400', '#FF0056'],
+                                  height=600,
+                                  title=f"Actual and Predict Prices for {', '.join(str(dropdown[i]) for i in selected_dropdown)} Over Time",
+                                  xaxis={"title": "Date",
+                                         'rangeselector': {'buttons': list([{'count': 1, 'label': '1M',
+                                                                             'step': 'month',
+                                                                             'stepmode': 'backward'},
+                                                                            {'count': 6, 'label': '6M',
+                                                                             'step': 'month',
+                                                                             'stepmode': 'backward'},
+                                                                            {'step': 'all'}])},
+                                         'rangeslider': {'visible': True}, 'type': 'date'},
+                                  yaxis={"title": "Price (USD)"})}
+    return figure
 
 
 if __name__ == '__main__':
